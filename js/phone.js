130e4e4b1ddc658d406e2433bd4d2175f8ed733c
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-08-29 02:13:51 +0600

    second

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-  const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadPhone = async (searchText) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
   const data = await res.json();
   const phones = data.data
   // console.log(phones)
@@ -10,11 +10,12 @@ const displayPhones = phones => {
   // console.log(phones)
   // create
   const phoneContainer=document.getElementById("phone-container")
+  phoneContainer.textContent=""
   phones.forEach(phone => {
-    console.log(phone)
+    // console.log(phone)
     // create a div
     const phoneCard = document.createElement('div')
-    phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`
+    phoneCard.classList = `card  bg-gray-100 shadow-xl p-4`
     // set innerhtml
     phoneCard.innerHTML = `
     <figure><img src="${phone.image}" alt="Shoes" /></figure>
@@ -28,5 +29,15 @@ const displayPhones = phones => {
 
     phoneContainer.appendChild(phoneCard)
   });
+
 }
-loadPhone()
+
+// hendel search button
+
+const handelSearch=()=>{
+  const serachField =document.getElementById('search-field')
+  const searchText=serachField.value
+  // console.log(searchText)
+  loadPhone(searchText)
+}
+// loadPhone()
