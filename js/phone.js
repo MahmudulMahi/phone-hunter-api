90c1cc796e047afff11b44fa30b12e6ecc365632
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-08-29 14:42:20 +0600

    third

--- js/phone.js
+++ js/phone.js
@@ -1,12 +1,12 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText,isShowAll) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
   const data = await res.json();
   const phones = data.data
   // console.log(phones)
-  displayPhones(phones)
+  displayPhones(phones,isShowAll)
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones,isShowAll) => {
   // console.log(phones)
   // create
   const phoneContainer=document.getElementById("phone-container")
@@ -15,15 +15,18 @@ const displayPhones = phones => {
   // display show all button
   const showAllContainer=document.getElementById('show-all-container')
 
-  if(phones.length >12){
+  if(phones.length >12 && !isShowAll){
     showAllContainer.classList.remove('hidden')
   }
   else{
     showAllContainer.classList.add('hidden')
   }
-
+  console.log("is show ",isShowAll)
   // display 10 phone
-  phones =phones.slice(0,10)
+
+  if(!isShowAll){
+    phones =phones.slice(0,12)
+  }
   phones.forEach(phone => {
     // console.log(phone)
     // create a div
@@ -36,27 +39,53 @@ const displayPhones = phones => {
       <h2 class="card-title">${phone.phone_name}</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
       <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+        <button onclick="handelShowDetail('${phone.slug}')" class="btn btn-primary">Buy Now</button>
       </div>
     </div>`
 
     phoneContainer.appendChild(phoneCard)
   });
 
+  toggleLoadSpinner(false)
+
+}
+
+const handelShowDetail= async (id)=>{
+  console.log('clicked',id)
+
+  const res =await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  const data =await res.json();
+  console.log(data)
 }
 
 // hendel search button
 
-const handelSearch=()=>{
+const handelSearch=(isShowAll)=>{
+  toggleLoadSpinner(true)
   const serachField =document.getElementById('search-field')
   const searchText=serachField.value
   // console.log(searchText)
-  loadPhone(searchText)
+  loadPhone(searchText,isShowAll)
 }
 
-const handelSearch2=()=>{
-  const serachField=document.getElementById('srarch-field2')
-  const searchText =serachField.value
-  loadPhone(searchText)
+// const handelSearch2=()=>{
+//   toggleLoadSpinner(true)
+//   const serachField=document.getElementById('srarch-field2')
+//   const searchText =serachField.value
+//   loadPhone(searchText)
+// }
+
+const toggleLoadSpinner=(isload)=>{
+  const loadingSpinner =document.getElementById('loading-spinner')
+  if(isload){
+    loadingSpinner.classList.remove('hidden')
+  }
+  else{
+    loadingSpinner.classList.add('hidden')
+  }
+}
+
+const handelShowAll=()=>{
+  handelSearch(true)
 }
 // loadPhone()
